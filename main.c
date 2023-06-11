3ab128893bcea7ce2a29812c4c8b0aed15d1d0c4
Nour Awawdeh <117414786+NOUR-AWAWDEH@users.noreply.github.com>
2023-06-11 12:28:16 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,32 +1,24 @@
 #include <stdio.h>
 #include <math.h>
-//beecrowd | 1038 : Snack
+// Beecrowd | 1038: Snack
 int main() 
 {
-    //X = array of prices , Y = quantity of the items 
-    int code ,quantity;
+    // X = array of prices, Y = quantity of the items 
+    int code, quantity;
     double amount;
-    double price[5] = {4.00,4.50,5.00,2.00,1.50};
-
-    if (scanf("%d%d",&code ,&quantity))
-        {
-            if (code <= 0 && code <= 6){
-
-            printf("Erorr\n");
-
-        }else{
-                
-                code -= 1;
-
-                amount = price[code] * quantity; 
-                
-                printf("Total: R$ %0.2lf\n",amount);
-            }
-
-    }else {
-
-       printf("Error");
+    double price[5] = {4.00, 4.50, 5.00, 2.00, 1.50};
+    
+    if (scanf("%d%d", &code, &quantity) == 2) {
+        if (code < 1 || code > 5) {
+            printf("Error\n");
+        } else {
+            code -= 1;  
+            amount = price[code] * quantity; 
+            printf("Total: R$ %.2lf\n", amount);
+        }
+    } else {
+        printf("Error\n");
     }
-        
- return 0; 
+    
+    return 0; 
 }
